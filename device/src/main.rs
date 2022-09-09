14598bef81045af780edc0ea1bf140b1ca74074b
Rob Tsuk <rob@tsuk.com>
2022-09-09 05:47:23 -0700

    Pixels on device

--- device/src/main.rs
+++ device/src/main.rs
@@ -1,14 +1,129 @@
+//! Uses RTIC with the RTC as time source to blink an LED.
+//!
+//! The idle task is sleeping the CPU, so in practice this gives similar power
+//! figure as the "sleeping_timer_rtc" example.
 #![no_std]
 #![no_main]
 
-use feather_m4 as hal;
-use hal::entry;
-use panic_semihosting as _;
-use rtt_target::{rprintln, rtt_init_print};
+use feather_m4 as bsp;
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Hello, world!");
-    loop {}
+use panic_semihosting as _;
+
+#[rtic::app(device = bsp::pac, peripherals = true, dispatchers = [EVSYS_0])]
+mod app {
+    use super::*;
+    use bsp::{hal, pin_alias};
+    use display_interface_spi::SPIInterface;
+    use embedded_graphics::{
+        mono_font::{ascii::FONT_6X10, MonoTextStyle},
+        pixelcolor::Rgb565,
+        prelude::*,
+        text::{Alignment, Text},
+    };
+    use hal::clock::GenericClockController;
+    use hal::pac::Peripherals;
+    use hal::prelude::*;
+    use ili9341::{DisplaySize240x320, Ili9341, Orientation};
+    use rtt_target::rtt_init_print;
+    use systick_monotonic::*;
+
+    /// Worlds worst delay function.
+    #[inline(always)]
+    pub fn delay_ms(ms: u32) {
+        const CYCLES_PER_MILLIS: u32 = SYSCLK_HZ / 1000;
+        cortex_m::asm::delay(CYCLES_PER_MILLIS.saturating_mul(ms));
+    }
+
+    const SYSCLK_HZ: u32 = 120_000_000;
+
+    pub struct CycleDelay;
+
+    impl Default for CycleDelay {
+        fn default() -> CycleDelay {
+            CycleDelay
+        }
+    }
+
+    impl embedded_hal::blocking::delay::DelayMs<u16> for CycleDelay {
+        fn delay_ms(&mut self, ms: u16) {
+            delay_ms(ms.into())
+        }
+    }
+
+    #[local]
+    struct Local {
+        red_led: bsp::RedLed,
+    }
+
+    #[shared]
+    struct Shared {}
+
+    #[monotonic(binds = SysTick, default = true)]
+    type RtcMonotonic = Systick<100>;
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        rtt_init_print!();
+
+        delay_ms(100);
+
+        let mono = Systick::new(cx.core.SYST, 120_000_000);
+        let mut peripherals: Peripherals = cx.device;
+        let pins = bsp::Pins::new(peripherals.PORT);
+        let red_led: bsp::RedLed = pin_alias!(pins.red_led).into();
+
+        let mut clocks = GenericClockController::with_internal_32kosc(
+            peripherals.GCLK,
+            &mut peripherals.MCLK,
+            &mut peripherals.OSC32KCTRL,
+            &mut peripherals.OSCCTRL,
+            &mut peripherals.NVMCTRL,
+        );
+
+        let sck = pins.sck;
+        let miso = pins.miso;
+        let mosi = pins.mosi;
+        let mclk = &mut peripherals.MCLK;
+
+        let lcd_dc = pins.d10.into_push_pull_output();
+        let lcd_cs = pins.d9.into_push_pull_output();
+
+        // Start the blink task
+        blink::spawn().unwrap();
+
+        let sercom = peripherals.SERCOM1;
+        let spi = bsp::spi_master(&mut clocks, 16.mhz(), sercom, mclk, sck, mosi, miso);
+        let spi_iface = SPIInterface::new(spi, lcd_dc, lcd_cs);
+        let reset_pin = pins.d12.into_push_pull_output();
+
+        let mut lcd = Ili9341::new(
+            spi_iface,
+            reset_pin,
+            &mut CycleDelay::default(),
+            Orientation::Landscape,
+            DisplaySize240x320,
+        )
+        .unwrap();
+
+        lcd.clear(Rgb565::BLUE).expect("clear");
+
+        let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+
+        Text::with_alignment(
+            "First line\nSecond line",
+            Point::new(160, 110),
+            style,
+            Alignment::Center,
+        )
+        .draw(&mut lcd)
+        .unwrap();
+
+        (Shared {}, Local { red_led }, init::Monotonics(mono))
+    }
+
+    #[task(local = [red_led])]
+    fn blink(cx: blink::Context) {
+        cx.local.red_led.toggle().unwrap();
+        blink::spawn_after(500.millis()).ok();
+    }
 }
